20aef948a31bdcadd270bdf7eedc074035962a35
jcejudo <joseed.cejudo@europeana.eu>
2024-04-23 15:52:12 +0200

    added downloading thumbnails for labeled dataset

--- scripts/download_images.py
+++ scripts/download_images.py
@@ -51,34 +51,49 @@ class HashDatabase:
         return objects
 
 
-def download_objects_IBM_function(row, suffix = 'MEDIUM'):
+def download_objects_IBM_function(row, output_path, suffix='MEDIUM'):
+    if 'category' in row:
+        category_path = output_path.joinpath(row['category'])
+    else:
+        category_path = output_path
+
+    category_path.mkdir(parents=True, exist_ok=True)
+
     prefix = row['ThumbnailID']
-    europeana_id = row['ProvidedCHO'].replace('/','[ph]')
-    object_summary_iterator = bucket.objects.filter(Prefix = prefix)
+    europeana_id = row['ProvidedCHO'].replace('/', '[ph]')
+    fname = europeana_id + '.jpg'
+    download_path = category_path.joinpath(fname)
+
+    object_summary_iterator = bucket.objects.filter(Prefix=prefix)
     for item in object_summary_iterator:
         try:
             object = client.Object("europeana-thumbnails-production", item.key)
             if object.key.endswith(suffix):
-                fname = europeana_id
-                object.download_file(output_path.joinpath(f'{fname}.jpg'))            
-        except:
-            pass
+                object.download_file(str(download_path))  # Ensure the path is string if necessary
+        except Exception as e:
+            print(f"Failed to download {item.key}: {e}")
 
-def download_objects_AWS_function(row, suffix = 'MEDIUM'):
-    bucket_name = 'europeana-thumbnails-production'
+def download_objects_AWS_function(row, output_path, suffix='MEDIUM'):
+    if 'category' in row:
+        category_path = output_path.joinpath(row['category'])
+    else:
+        category_path = output_path
+
+    category_path.mkdir(parents=True, exist_ok=True)
+
     prefix = row['ThumbnailID']
-    europeana_id = row['ProvidedCHO'].replace('/','[ph]')
+    europeana_id = row['ProvidedCHO'].replace('/', '[ph]')
+    fname = europeana_id + '.jpg'
+    download_path = category_path.joinpath(fname)
 
-    object_summary_iterator = s3.list_objects_v2(Bucket=bucket_name, Prefix=prefix)
+    object_summary_iterator = s3.list_objects_v2(Bucket='europeana-thumbnails-production', Prefix=prefix)
     for item in object_summary_iterator.get('Contents', []):
         try:
             file_name = item['Key']
             if file_name.endswith(suffix):
-                fname = europeana_id
-                download_path = output_path.joinpath(f'{fname}.jpg')
-                s3.download_file(bucket_name, file_name, download_path)
-        except:
-            pass
+                s3.download_file('europeana-thumbnails-production', file_name, str(download_path))
+        except Exception as e:
+            print(f"Failed to download {file_name}: {e}")
 
 
 def process_file(entry):
@@ -131,8 +146,14 @@ if __name__ == "__main__":
     hash_df = pd.DataFrame.from_dict(objects)
     print(f'Number of hashes found: {hash_df.shape[0]}')
 
-    dowloaded_ids = get_downloaded_ids(output_path)
-    hash_df = hash_df.loc[hash_df['ProvidedCHO'].apply(lambda x: x not in dowloaded_ids)]
+    #dowloaded_ids = get_downloaded_ids(output_path)
+    #hash_df = hash_df.loc[hash_df['ProvidedCHO'].apply(lambda x: x not in dowloaded_ids)]
+
+    provided_cho_set = set(hash_df['ProvidedCHO'])
+    input_df = input_df[input_df['europeana_id'].isin(provided_cho_set)]
+    if 'category' in input_df.columns:
+        hash_df = hash_df.merge(input_df[['europeana_id', 'category']], left_on='ProvidedCHO', right_on='europeana_id', how='left')
+
     print(f'Number of images to download: {hash_df.shape[0]}')
 
     # Search IBM 
@@ -150,11 +171,14 @@ if __name__ == "__main__":
     )
 
     bucket = client.Bucket('europeana-thumbnails-production')
-    rows = [row for index, row in hash_df.iterrows()]
+    rows = hash_df.to_dict('records')
     
     start_time = time.perf_counter()
-    with Pool(processes=processes) as pool:
-      result = list(tqdm(pool.imap(partial(download_objects_IBM_function,suffix=suffix), rows)))
+    with Pool(processes=args.processes) as pool:
+        partial_func = partial(download_objects_IBM_function, output_path=output_path, suffix=args.suffix)
+        list(tqdm(pool.imap(partial_func, rows), total=len(rows), desc="Downloading Images from IBM"))
+
+
     finish_time = time.perf_counter()
     ibm_time = (finish_time-start_time)/60.0
     print("Finished, it took {} minutes".format(ibm_time))
@@ -177,11 +201,13 @@ if __name__ == "__main__":
                     aws_access_key_id=aws_access_key_id, 
                     aws_secret_access_key=aws_secret_access_key)
 
-    rows = [row for index, row in hash_df.iterrows()]
+    rows = hash_df.to_dict('records')
     
     start_time = time.perf_counter()
-    with Pool(processes=processes) as pool:
-      result = list(tqdm(pool.imap(partial(download_objects_AWS_function,suffix=suffix), rows)))
+    with Pool(processes=args.processes) as pool:
+        partial_func = partial(download_objects_AWS_function, output_path=output_path, suffix=args.suffix)
+        list(tqdm(pool.imap(partial_func, rows), total=len(rows), desc="Downloading Images from IBM"))
+
     finish_time = time.perf_counter()
     aws_time = (finish_time-start_time)/60.0
     print("Finished, it took {} minutes".format(aws_time))
